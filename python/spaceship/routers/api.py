0d550cdc460e3876da48c90e490df37e42266268
elaiviaien <skrypnyk.michael@gmail.com>
2025-05-22 11:42:05 +0300

    add numpy

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,21 @@
 from fastapi import APIRouter
-
+import numpy as np
 router = APIRouter()
 
 
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("/matrix-multiply")
+def multiply_matrices() -> dict:
+    matrix_a = np.random.randint(0, 10, size=(10, 10)).tolist()
+    matrix_b = np.random.randint(0, 10, size=(10, 10)).tolist()
+    product = (np.dot(np.array(matrix_a), np.array(matrix_b))).tolist()
+
+    return {
+        "matrix_a": matrix_a,
+        "matrix_b": matrix_b,
+        "product": product
+    }
